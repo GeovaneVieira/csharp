dc45ef477f468297f1bf66b99f25f7d61d14afab
Geovane Vieira <geovane.vieira@gmail.com>
2017-08-02 14:38:01 -0300

    Início

---
+++ Delegates/ConsoleApplication1/ConsoleApplication1/DelegateTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public delegate string FirstDelegate(int x);
+    class DelegateTest
+    {
+        string name;
+
+        public string InstanceMethod(int i) 
+        {
+            return string.Format("{0}: {1}", name, i);
+        }
+
+        public string StaticMethod(int i)
+        {
+            return string.Format("Static method: {0}", i);
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
