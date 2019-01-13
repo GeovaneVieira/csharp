3bee491956f4f3b9af508eb98c7c90103a50d7bc
Geovane Vieira <geovane.vieira@gmail.com>
2019-01-13 12:01:04 -0200

    Novos projetos

---
+++ DotLab/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// The code provided will print ‘Hello World’ to the console.
+			// Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+			Class1 teste = new Class1();
+
+			Console.WriteLine("Hello World!");
+			Console.ReadKey();
+
+			// Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+		}
+	}
+}
