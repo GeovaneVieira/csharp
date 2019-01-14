07c5202f8b21ebd4d1a1a04ea4f0c9daec12c1c0
Geovane Vieira <geovane.vieira@gmail.com>
2019-01-14 21:42:38 -0200

    Continuação

--- udemy/dotnetCore/Hosting/HostingExample/Program.cs
+++ udemy/dotnetCore/Hosting/HostingExample/Program.cs
@@ -11,13 +11,24 @@ namespace HostingExample
         {
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .Configure(app => {
-                    app.Run(context => context.Response.WriteAsync("Olá mundo!"));
-                })
+                // .Configure(app => {
+                //     app.Run(context => context.Response.WriteAsync("Olá mundo!"));
+                // })
+                .UseStartup<Startup>()
                 .Build()
             ;
             
             host.Run();
+        }
+    }
+
+    internal class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) => {
+                await context.Response.WriteAsync("Trabalhando com a classe Startup");
+            });
         }
     }
 }
